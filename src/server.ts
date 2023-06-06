95fbd46cac20797f8b28a59420aa57d320eea167
camille <lc.laignel@gmail.com>
2023-06-06 11:55:16 +0200

    Refactor: Improved error handling for database and server operations

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,36 @@
 import dotenv from "dotenv";
 import app from "./main";
 import connectToDB from "./shared/services/connectDb.service";
+import http from "http";
+import { nodeEnv } from "./shared/types/types";
 
+let server: http.Server;
 dotenv.config({ path: "./config.env" });
+
+const nodeEnv = process.env.NODE_ENV as nodeEnv;
 
 const { DATABASE, DATABASE_PASSWORD, PORT } = process.env;
 
 const databaseUri = DATABASE?.replace("<password>", DATABASE_PASSWORD!);
 
 async function startServer() {
-  try {
-    await connectToDB(databaseUri!);
-    const port = PORT || 3000;
-    app.listen(port, () => {
-      console.log(`✅ Server is listening on port ${port}`);
+  await connectToDB(databaseUri!);
+  const port = PORT || 3000;
+  server = app.listen(port, () => {
+    console.log(`✅ Server is listening on port ${port}`);
+  });
+
+  server.on("error", (error: any) => {
+    if (nodeEnv === "development") {
+      console.error("💥 Server startup error:", error);
+    } else if (nodeEnv === "production") {
+      console.error("💥 Server startup error:", error.name, error.message);
+    }
+
+    server.close(() => {
+      process.exit(1);
     });
-  } catch (error) {
-    console.error("💥 Server startup error:", error);
-  }
+  });
 }
 
 startServer();
